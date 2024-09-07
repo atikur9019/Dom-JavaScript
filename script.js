52e506b08c6337fe9e15f25f20e3e02d333b189c
Atikur Rhaman <designletter999@gmail.com>
2024-09-07 10:05:16 +0600

    add file & add code-2

---
+++ script.js
@@ -0,0 +1,45 @@
+// console.log('hello!');
+// console.log(window);
+// window.console.log('hello!');
+// window.alert('hello!');
+// alert('hello!');
+
+// let heading = document.getElementById("headings"); //h1
+// console.log(heading);
+
+// let button = document.getElementById("myId"); //button
+// console.log(button);
+
+
+// let headings = document.getElementsByClassName("myClass");
+// console.dir(headings);
+// console.log(headings);
+
+// let paras = document.getElementsByTagName("p");
+// console.dir(paras);
+
+
+// let firstEL = document.querySelector("p"); //1st element
+// let firstElClass = document.querySelector(".myClass"); //1st element
+// let firstElId = document.querySelector("#myId"); //1st element
+// console.dir(firstEL);
+
+// let allEl = document.querySelectorAll("p"); //all elements
+// console.dir(allEl);
+
+// let firstElId = document.querySelector("#myId");
+// let tagName = firstElId.tagName;
+// console.log(tagName);
+
+// console.dir(firstElId);
+
+// console.log(document.body.firstChild);
+
+// console.log(document.querySelector("div").children);
+
+
+
+let div = document.querySelector("div");
+console.dir(div);
+
+let heading = document.querySelector("h1");
